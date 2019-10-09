e3ceda4b498d4fb995de2ec72279bb453da7661f
Robin-wushuo <wushuowow@gmail.com>
2019-10-09 16:39:44 +0800

    design.py: change Bridge DP to dependency injection
    There is a problem with last version of design.py, in fact, Bridge DP
    is not suitable for this script. In Bridge, there are more than one
    interface, but this is not necessary for design.py. Because there is no
    requirement on interface, so read and write methods can be contained in
    just one class. Then dependency injection would supply extensibility.

--- design.py
+++ design.py
@@ -1,58 +1,62 @@
-"""License boilerplate."""
+# This is a script, so it needs a script docstring.
+"""usage: see the argparse module in python standard library."""
 
 
-class AbstractIO(object):
+class Transmitor(object):
+    """For extrating text.
 
-    def __init__(self, file):
-        self.file = file
+    Methods:
+        transmit: Transmit the text between files.
+    """
 
+    def __init__(self, fromfile, tofile):
+        """Dependency injection.
 
-class Reader(AbstractIO):
-    """Reading interface of Bridge DP."""
-
-    def read(self):
-        """Delegates to read method of implementor class.
-
-        Extracts unicode contents from file.
-        Returns:
-            A string of contents.
+        Args:
+            fromfile: An implementor of different file format.
+            tofile: Another implementor of different file format.
         """
-        return self.file.read()
+        self.fromfile = fromfile
+        self.tofile = tofile
+        self.text = ''
+
+
+    def transmit(self, pattern, pages):
+        """Finds the text from a file then insert it into another file.
+
+        Args:
+            pattern: A regular expression pattern.
+            pages: Counts of pages to be read.
+        """
+        self.text = self.fromfile.gettext(pages)
+        # This needs additional effort in order to work.
+        self.extract =  re.find(pattern)
+        self.tofile.insert(self.extract)
 
 
 class PdfFile(object):
-    """PDF implementor of Bridge."""
+    """PDF dependency."""
 
-    def read(self):
-        """See read method of Reader class."""
+    def gettext(self, pages):
+        """Gets text from pdf file by tika.
 
+        Args:
+            pages: see transmit method of Transmitor.
+        """
         # TODO(tika@github.com): Learn the package.
 
 
 class WordFile(object):
-    """MS word implementor of Bridge."""
+    """MS word dependency."""
 
-    def read(self):
-        """See read method of Reader class."""
-
+    def gettext(self):
+        """Gets text from word file by some module."""
         # TODO(Robin) Find a capable word package.
 
 
-class Writer(AbstractIO):
-    """Writing interface of Bridge DP."""
-
-    def write(self):
-        """Delegates to write method of implementor class.
-
-        Inserts unicode contents into file.
-        """
-        self.file.write()
-
-
 class ExcelFile(object):
-    """MS excel implementor of Bridge."""
+    """MS excel dependency."""
 
-    def write(self):
-        """See write method of Writer class."""
-
+    def insert(self):
+        """Inserts text into excel file."""
         # TODO(Robin) Find a capable excel pakcage.
