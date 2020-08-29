44e57efbd284a742663ad1123191a49cbe3eae37
Robin-wushuo <wushuowow@gmail.com>
2020-08-29 11:04:26 +0800

    Handle scanned pdf.

--- ee.py
+++ ee.py
@@ -1,19 +1,27 @@
 # This is a script, so it needs a script docstring.
 """Usage: see the argparse module in python standard library."""
 
-from collections import namedtuple
 import configparser
 import concurrent.futures
 import io
 import itertools
+import math
+import os
 import re
+import tempfile
 import zipfile
 
+from collections import namedtuple
+
 import pandas as pd
+
 from tika.parser import from_buffer as tika_parse
 from fuzzywuzzy import fuzz
 from fuzzywuzzy.process import extractOne
 from xlsxwriter.utility import xl_rowcol_to_cell
+from pytesseract import image_to_data, Output
+from PIL import Image
+
 
 config = configparser.ConfigParser()
 config.read('config.ini')
@@ -30,7 +38,7 @@ with pd.ExcelFile('test/template.xlsx') as xls:
     xls.sheet_names.remove('template')
     extra_data = pd.read_excel(xls, xls.sheet_names)
     data_validation = extra_data.get('valid')
-    row_match = template.loc['Match_info'].dropna()
+    MATCHINFO = template.loc['Match_info'].dropna()
 
 
 def schedule(g):
@@ -83,8 +91,8 @@ def block_for_future(future):
     block(get_future_queue(future))
 
 
-def async_func(pool, function, *args):
-    future = pool.submit(function, *args)
+def wait(pool, function, *args, **kwargs):
+    future = pool.submit(function, *args, **kwargs)
     block_for_future(future)
     yield
     return future.result()
@@ -114,37 +122,124 @@ def _cut(text, pages, page_break='\n\n\n '):
     return text[:cut_point]
 
 
-def _remove_footnote(text):
-    m = re.search(config['noheader_pattern']['text_with_footnote'], text)
-    if m:
-        footnote_pat = re.compile(
-                repr(m.group('f1'))[1:-1]
-                + r'\d?\d ' + repr(m.group('f2'))[1:-1])
-        # Keeps policy which is inside the footnote.
-        policy = footnote_pat.pattern.lower().replace(' policy', '\npolicy')
-        text = policy + footnote_pat.sub('', text)
+# def _remove_footnote(text):
+#     m = re.search(config['noheader_pattern']['text_with_footnote'], text)
+#     if m:
+#         footnote_pat = re.compile(
+#                 repr(m.group('f1'))[1:-1]
+#                 + r'\d?\d ' + repr(m.group('f2'))[1:-1])
+#         # Keeps policy which is inside the footnote.
+#         policy = footnote_pat.pattern.lower().replace(' policy', '\npolicy')
+#         text = policy + footnote_pat.sub('', text)
+#     return text
+
+# Change wait function to decorator.
+def pdfimage_string(filename):
+    abs_path = os.path.abspath(filename)
+    dirname = os.path.dirname(abs_path)
+    with tempfile.TemporaryDirectory(dir=dirname) as image_dir:
+        image_dir = os.path.join(image_dir, '')
+        cmd = 'pdfimages -l 9 -tiff {} {}'.format(filename, image_dir)
+        yield from wait(p_pool, os.system, cmd)
+        with os.scandir(image_dir) as it:
+            images = [x.path for x in it]
+            images.sort()
+        img = image_processing(images.pop(0))
+        text = yield from image_string(img)
+        for image_path in images:
+            text += yield from image_string(image_path)
     return text
 
 
-def parse(pdf_file, cut_page=True):
-    byte_string = yield from read(pdf_file)
-    parsed = yield from async_func(t_pool, tika_parse, byte_string)
-    text = parsed['content']
-    if text:
-        text = text.lstrip('\n')
-    else:
-        print('\nFailed: %s is scanned pdf.' % pdf_file.name)
-        return ''
-    if cut_page:
-        text = _cut(text, pages=9)
-    text = yield from async_func(p_pool, _remove_footnote, text)
+import noteshrink
+from PIL import Image
+import numpy as np
+def image_processing(filename):
+    parser = noteshrink.get_argument_parser()
+    options = parser.parse_args([filename, '-s', '5', '-n', '2'])
+    print('opened', filename)
+    print('  saving {}...'.format(filename))
+    img, dpi = noteshrink.load(filename)
+    root, ext = os.path.splitext(filename)
+    output_filename = '{}_processed{}'.format(root, ext)
+    samples = noteshrink.sample_pixels(img, options)
+    palette = noteshrink.get_palette(samples, options)
+    quantized = noteshrink.quantize(img, bits_per_channel=3).astype(np.uint8)
+    labels = noteshrink.apply_palette(quantized, palette, options)
+    output_img = Image.fromarray(labels, 'P')
+    output_img.putpalette(palette.flatten())
+    print('  done\n')
+    return output_img
+
+
+def image_string(image):
+    # TODO: tesseract table layout analysis.
+    # TODO: OMP_THREAD_LIMIT=1
+    # TODO: threshold image.
+    # TODO: learn converting RGB to HSV@/copa/bg/tesseract/a 3D.
+    # TODO: What is subprocess.call?
+    df = yield from wait(
+            p_pool, image_to_data, image, output_type=Output.DATAFRAME)
+
+    # Sorts lines by 'top' and 'left'.
+    columns = ['block_num', 'par_num', 'line_num']
+    grouped = df.groupby(columns)
+    line = grouped.head(1)
+    line = line.query('line_num != 0 & width > 50 & height > 10')
+    line = line.copy(deep=False)
+    line.loc[:, 'top'] = line.top.apply(has_close_value(line.top))
+    order_line = line.sort_values(['top', 'left'])
+
+    # Reorder. block_num is prior to top in first column of table.
+    page = df.query('level == 1')
+    duplicates = order_line[order_line.duplicated('top', keep=False)]
+    bins = list(page.top) + list(duplicates.top.unique()) + list(page.height)
+    order_line.loc[:, 'group'] = pd.cut(order_line.top, bins=bins, right=False)
+    reorder = pd.DataFrame()
+    grouped = order_line.groupby('group')
+    for name, group in grouped:
+        duplicate = duplicates[duplicates.top == name.left]
+        pair = group[group.block_num.isin(duplicate.block_num)]
+        other = group[~group.block_num.isin(duplicate.block_num)]
+        try:
+            pair_group = pair.groupby('block_num')
+        except KeyError:
+            pass
+        else:
+            for _, grp in pair_group:
+                reorder = reorder.append(grp)
+        reorder = reorder.append(other)
+
+    # Gets text from the ordered dataframe.
+    order_line = reorder.loc[:, columns]
+    order_df = order_line.merge(df).fillna('\n')
+    text = ' '.join(order_df.text).replace('\n ', '\n')
     return text
 
 
-def read(file):
-    data = yield from async_func(t_pool, file.read)
-    file.close()
-    return data
+# TODO: Learn: "Determining the number of clusters in a data set"@wiki
+# TODO: Learn: "k-means clustering"@wiki
+# TODO: Replaced by noteshrink.quantize
+def has_close_value(num_list):
+    groups = set()
+    def _isclose(number):
+        nonlocal num_list
+        # Finds in exist groups.
+        for min_value, max_value in groups:
+            if (math.isclose(min_value, number, abs_tol=10)
+                    or math.isclose(max_value, number, abs_tol=10)):
+                return max_value
+        # Finds in num_list.
+        choices = list(num_list)
+        findings = [number]
+        for num in choices:
+            if math.isclose(num, number, abs_tol=10):
+                findings.append(num)
+        boundary = (min(findings), max(findings))
+        groups.add(boundary)
+        num_list = filter(lambda x: x<boundary[0] or x>boundary[1], choices)
+        return boundary[1]
+    return _isclose
 
 
 def get_alias(name):
@@ -208,16 +303,16 @@ def fetch(info, text, dataframe, header=True):
                 # Find value from dataframe by fuzzywuzzy.
                 source = extra_data.get(c, data_validation)
                 choices = source[info.column]
-                first_group = m.group(info.groups[0])
-                best_match = standard_match(first_group, choices.dropna())
-                matched_value, ratio, _ = best_match
+                to_match = dataframe.at['value', info.column]
+                best_match = standard_match(to_match, choices.dropna())
+                matched, ratio, _ = best_match
                 if ratio < 80:
-                    print('Match(%s)\nResult%s' % (first_group, best_match))
-                    print('Location(%s in %s)' % (info.column, info.filename))
+                    print('Match(%s)\nResult(%s)' % (to_match, matched))
+                    print('Location(%s @%s)\n' % (info.column, info.filename))
                 if c == info.column:
-                    dataframe.at['value', c] = matched_value
+                    dataframe.at['value', c] = matched
                 else:
-                    selection = source[choices == matched_value]
+                    selection = source[choices == matched]
                     dataframe.at['value', c] = selection.iloc[0][c]
             else:
                 dataframe.at['value', c] = adjust(value)
@@ -243,7 +338,7 @@ def standard_match(value, choices):
     return match
 
 
-def format_then_to_excel(bio, dataframe):
+def to_formatted_excel(bio, dataframe):
     with pd.ExcelWriter(bio, engine='xlsxwriter') as writer:
         # Write worksheet1 and worksheet2.
         dataframe.to_excel(writer, index=False)
@@ -275,20 +370,19 @@ class Crawler(object):
     def __init__(self, zip):
         self.zip = zipfile.ZipFile(zip, mode='a')
         self.max_worker = 3
-        self.pdf_files = []
+        self.pdf_names = []
         self.data = {}
 
     def crawl(self):
         for filename in self.zip.namelist():
             if filename.endswith('pdf'):
-                pdf = self.zip.open(filename)
-                self.pdf_files.append(pdf)
+                self.pdf_names.append(filename)
         for _ in range(self.max_worker):
             schedule(self.work())
         run()
         for name, dataframe in self.data.items():
             bio = io.BytesIO()
-            format_then_to_excel(bio, dataframe)
+            to_formatted_excel(bio, dataframe)
             bio.seek(0)
             with open(name.replace('.pdf', '.xlsx'), mode='wb') as file:
                 file.write(bio.read())
@@ -296,20 +390,50 @@ class Crawler(object):
         self.zip.close()
 
     def work(self):
-        while self.pdf_files:
-            file = self.pdf_files.pop(0)
-            text = yield from parse(file)
+        while self.pdf_names:
+            filename = self.pdf_names.pop()
+            text = yield from self.parse(filename)
             if not text:
                 continue
             dataframe = template['value':'value'].applymap(_get_function)
-            self.data[file.name] = dataframe
-            for match_info in handle(row_match):
-                match_info = match_info._replace(filename=file.name)
-                schedule(self.update(dataframe, text, match_info))
+            self.data[filename] = dataframe
+            for match in handle(MATCHINFO):
+                match = match._replace(filename=filename)
+                schedule(self.dataframe_update(dataframe, text, match))
 
-    def update(self, dataframe, text, info):
-        df = yield from async_func(p_pool, fetch, info, text, dataframe)
-        if dataframe.equals(df):
-            print('Warning: %s not found in %s.' % (info.column, info.filename))
+    def dataframe_update(self, dataframe, text, info):
+        df = yield from wait(p_pool, fetch, info, text, dataframe)
+        if dataframe[info.column].equals(df[info.column]):
+            print('Warning: %s: %s failed.' % (info.filename, info.column))
         df = df.loc[:, df.columns.isin(info.columns)]
         dataframe.update(df)
+
+    def parse(self, filename, cut_page=True):
+        byte_string = yield from self.read(filename)
+        parsed = yield from wait(t_pool, tika_parse, byte_string)
+        text = parsed['content']
+        if text:
+            text = text.lstrip('\n')
+            if cut_page:
+                text = _cut(text, pages=9)
+        else:
+            with tempfile.TemporaryDirectory(dir='.') as dirname:
+                self.zip.extract(filename, dirname)
+                path = os.path.join(dirname, filename)
+                text = yield from pdfimage_string(path)
+
+        # text = yield from wait(p_pool, _remove_footnote, text)
+        return text
+
+    def read(self, filename):
+        with self.zip.open(filename) as pdf_file:
+            data = yield from wait(t_pool, pdf_file.read)
+        return data
+
+
+def test():
+    c = Crawler('my.zip')
+    c.crawl()
+
+if __name__ == '__main__':
+    test()
